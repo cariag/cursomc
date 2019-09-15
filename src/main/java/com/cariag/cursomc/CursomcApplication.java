431b91c5c19885ed3937917b1778e64d97f5153c
Guilherme Caria <guilhermecaria1@gmail.com>
2019-09-15 14:23:45 -0300

    Estadp e cidade

--- src/main/java/com/cariag/cursomc/CursomcApplication.java
+++ src/main/java/com/cariag/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cariag.cursomc.domain.Categoria;
+import com.cariag.cursomc.domain.Cidade;
+import com.cariag.cursomc.domain.Estado;
 import com.cariag.cursomc.domain.Produto;
 import com.cariag.cursomc.repositories.CategoriaRepository;
+import com.cariag.cursomc.repositories.CidadeRepository;
+import com.cariag.cursomc.repositories.EstadoRepository;
 import com.cariag.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -43,6 +51,21 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
 	}
 
 }
---
+++ src/main/java/com/cariag/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.cariag.cursomc.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cariag.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
