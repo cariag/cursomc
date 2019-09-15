7ce1cfa45f201fabdd00cfb2e25802b328cf38cc
Guilherme Caria <guilhermecaria1@gmail.com>
2019-09-15 08:51:05 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/cariag/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.cariag.cursomc.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cariag.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
