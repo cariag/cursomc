5a2452a7a14334fd81780cd015c98fafe4a844a6
Guilherme Caria <guilhermecaria1@gmail.com>
2019-09-16 06:11:28 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/cariag/cursomc/CursomcApplication.java
+++ src/main/java/com/cariag/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.cariag.cursomc.domain.Cidade;
 import com.cariag.cursomc.domain.Cliente;
 import com.cariag.cursomc.domain.Endereco;
 import com.cariag.cursomc.domain.Estado;
+import com.cariag.cursomc.domain.ItemPedido;
 import com.cariag.cursomc.domain.Pagamento;
 import com.cariag.cursomc.domain.PagamentoComBoleto;
 import com.cariag.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.cariag.cursomc.repositories.CidadeRepository;
 import com.cariag.cursomc.repositories.ClienteRepository;
 import com.cariag.cursomc.repositories.EnderecoRepository;
 import com.cariag.cursomc.repositories.EstadoRepository;
+import com.cariag.cursomc.repositories.ItemPedidoRepository;
 import com.cariag.cursomc.repositories.PagamentoRepository;
 import com.cariag.cursomc.repositories.PedidoRepository;
 import com.cariag.cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -115,6 +119,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }
---
+++ src/main/java/com/cariag/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,13 @@
+package com.cariag.cursomc.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cariag.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
